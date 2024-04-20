5bb5a9b1e465326a81f4e6eac96d0bc0c633aa8e
Felleria <tellytabbie@gmail.com>
2024-04-20 16:59:34 +0300

    commits

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,12 +1,20 @@
-// The code below ensures that students who are using CodeGrade will get credit
-// for the code-along in Canvas; you can disregard it.
+const init = () => {
+  const inputForm = document.querySelector("form");
 
-require("./helpers.js");
+  inputForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const input = document.querySelector("input#searchByID");
 
-describe("", () => {
-  describe("", () => {
-    it("Test passing", () => {
-      return true;
-    });
+    fetch(`http://localhost:3000/movies/${input.value}`)
+      .then((response) => response.json())
+      .then((data) => {
+        const title = document.querySelector("section#movieDetails h4");
+        const summary = document.querySelector("section#movieDetails p");
+
+        title.innerText = data.title;
+        summary.innerText = data.summary;
+      });
   });
-});
+};
+
+document.addEventListener("DOMContentLoaded", init);
